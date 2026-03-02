a67ce1571f674a85320d4ca36eecca7a6c0de2db
harsha7075226822 <ugadiharshavardhan@gmail.com>
2026-03-02 17:20:21 +0530

    feat: create config.js to define the backend URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-// export const BACKEND_URL = "https://hacknext.onrender.com";
-export const BACKEND_URL = "http://localhost:5678";
+export const BACKEND_URL = "https://hacknext.onrender.com";
+// export const BACKEND_URL = "http://localhost:5678";
 
